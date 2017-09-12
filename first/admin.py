67f45263c75a4041b7b7615be88e5ea505f26e36
Pavel Zagrebelin <pavel@zagrebelin.ru>
2017-09-13 01:12:51 +0500

    display list of dep members as RO list

--- first/admin.py
+++ first/admin.py
@@ -1,24 +1,23 @@
 from django import forms
 from django.contrib import admin
+from django.utils.html import format_html_join
+from django.utils.safestring import mark_safe
 
 from .models import Person, Departament, Company
 
 
-# code stolen here: https://stackoverflow.com/a/19556353
-class DepartamentAdminForm(forms.ModelForm):
-    class Meta:
-        model = Departament
-        fields = ['title', 'boss']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if self.instance:
-            self.fields['boss'].queryset = Person.objects.filter(departament=self.instance)
-
-
 class DepartamentInlineAdmin(admin.TabularInline):
-    form = DepartamentAdminForm
     model = Departament
+    readonly_fields = ['get_departament_names']
+
+    # https://docs.djangoproject.com/en/1.11/ref/contrib/admin/#django.contrib.admin.ModelAdmin.readonly_fields
+    def get_departament_names(self, instance):
+        return format_html_join(mark_safe('<br/>'),
+                                '{}',
+                                (line for line in instance.person_set.values_list('name')),
+                                )
+
+    get_departament_names.short_description = "List of dep members"
 
 
 class PersonInlineAdmin(admin.TabularInline):
@@ -26,5 +25,5 @@ class PersonInlineAdmin(admin.TabularInline):
 
 
 admin.site.register(Company, inlines=[DepartamentInlineAdmin])
-admin.site.register(Departament, inlines=[PersonInlineAdmin], form=DepartamentAdminForm)
+admin.site.register(Departament, inlines=[PersonInlineAdmin])
 admin.site.register(Person)
